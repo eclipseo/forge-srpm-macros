9b79de3f972dc90197f7c8301f901e3b78527a6d
Maxwell G <maxwell@gtmx.me>
2023-03-27 17:35:16 -0500

    tests: add test_slash_tags

--- tests/test_forge.py
+++ tests/test_forge.py
@@ -15,8 +15,9 @@ def test_github_forgesource_simple(evaluater):
     defines = {"forgeurl": forgeurl, "version": "2.14.0"}
 
     out = evaluater(["%forgemeta", "%forgesource"], defines)
-    expected = f"{forgeurl}/archive/v2.14.0/ansible-2.14.0.tar.gz"
-    assert out[0] == expected
+    sourceurl = f"{forgeurl}/archive/v2.14.0/ansible-2.14.0.tar.gz"
+    assert out[0] == sourceurl
+    return sourceurl
 
 
 @pytest.mark.parametrize(
@@ -37,6 +38,7 @@ def test_gitlab_forgesource_archiveext(archiveext, evaluater):
 
     sourceurl = f"{forgeurl}/-/archive/1.16.2/fdroidclient-1.16.2.{archiveext}"
     assert out[0] == sourceurl
+    return sourceurl
 
 
 def test_sourcehut_v(evaluater):
@@ -68,6 +70,7 @@ def test_sourcehut_v(evaluater):
         "-n fedrq-v0.5.0 :: -n fedrq-v0.5.0"
     )
     assert out[0] == expected
+    return sourceurl
 
 
 def test_github_commit(evaluater):
@@ -92,6 +95,7 @@ def test_github_commit(evaluater):
         f"-n ansible-{commit} :: -n ansible-{commit}"
     )
     assert out[0] == expected
+    return sourceurl
 
 
 @pytest.mark.parametrize(
@@ -102,9 +106,49 @@ def test_github_commit(evaluater):
     ],
 )
 def test_gitea_codeberg_simple_v(evaluater, forgeurl, version):
-    # Test that trailing slash works
+    # Test that trailing slash in %forgeurl works
     defines = {"forgeurl": forgeurl + "/", "version": version, "tag": "v%{version}"}
     out = evaluater(["%forgemeta", "%{forgesource} :: %{forgesetupargs}"], defines)
 
     sourceurl = f"{forgeurl}/archive/v{version}.tar.gz"
     assert out[0] == f"{sourceurl} :: -n {os.path.basename(forgeurl)}"
+    return sourceurl
+
+
+@pytest.mark.parametrize(
+    "forgeurl, tag, sourceurl, topdir",
+    [
+        pytest.param(
+            "https://git.sr.ht/~gotmax23/golang-x-tools",
+            "gopls/v0.9.0",
+            "{forgeurl}/archive/{tag}.tar.gz#/golang-x-tools-gopls-v0.9.0.tar.gz",
+            "golang-x-tools-gopls/v0.9.0",
+            id="git.sr.ht"
+        ),
+        pytest.param(
+            "https://github.com/golang/tools",
+            "gopls/v0.9.0",
+            "{forgeurl}/archive/{tag}/tools-gopls-v0.9.0.tar.gz",
+            "tools-gopls-v0.9.0",
+            id="github.com"
+        ),
+        pytest.param(
+            "https://gitea.com/gitea/go-sdk",
+            "gitea/v0.15.1",
+            "{forgeurl}/archive/{tag}.tar.gz#/gitea-v0.15.1.tar.gz",
+            "go-sdk",
+            id="gitea.com"
+        ),
+    ],
+)
+def test_slash_tags(evaluater, forgeurl, tag, sourceurl, topdir):
+    # Set a bogus version to make sure it doesn't impact results
+    defines = {"forgeurl": forgeurl, "tag": tag, "version": "100.0.2"}
+    for key, value in defines.items():
+        defines[key] = value.format(**defines)
+    out = evaluater(["%forgemeta", "%{forgesource} :: %{forgesetupargs}"], defines)
+
+    sourceurl = sourceurl.format(**defines)
+    topdir = topdir.format(**defines)
+    assert out[0] == f"{sourceurl} :: -n {topdir}"
+    return sourceurl
